cbc733058b9993c04216d29dfb7fa4c69570b6ca
jbratcher <jeremybratcher@gmail.com>
2018-07-22 12:42:59 -0400

    feat: added randomized character images based on gender selection

--- dist/ts/main.ts
+++ dist/ts/main.ts
@@ -1,137 +1,186 @@
 
-  // create function to get ability score 3-18
 
-  const abilityScore = function(){
-    return Math.floor(Math.random() * ((18 - 3) + 1)) + 3;
-  }
+// Character image array
 
-  const setToMinMax = score => score > 18 ? score = 18 : score = 3;
+const maleCharacterImages = [
+  "https://i.pinimg.com/736x/86/84/32/868432784e4fd37beb893ecdb0830a74--old-friends-barbarian-fantasy.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWmOrOMDeUgAgx9IWjCZD9g5oBec8-fk8xZH9UuofS_VAOt3zb",
+  "https://i.pinimg.com/736x/08/69/71/086971ec519d01df20ae9cb4bf5e411d--character-outfits-character-portraits.jpg",
+  "https://www.videogamesartwork.com/sites/default/files/images/image/1377178542/diablo3-class-barbarian-male-06.jpg",
+  "https://i.pinimg.com/736x/bd/4f/b9/bd4fb957e6046747533e6d62af90f6fe--fantasy-warrior-fantasy-art.jpg"
+]
 
-  // Declare Variables
+const femaleCharacterImages = [
+  "https://vignette.wikia.nocookie.net/bgtscc/images/9/99/Barbarian.jpg/revision/latest?cb=20161105214456",
+  "https://i.kinja-img.com/gawker-media/image/upload/s--NdbChKcZ--/c_scale,f_auto,fl_progressive,q_80,w_800/18j3zwhxy85iyjpg.jpg",
+  "https://cdna.artstation.com/p/assets/covers/images/001/568/198/large/denny-ibnu-barbarian-final-lr.jpg?1448720934",
+  "https://i.pinimg.com/236x/30/9a/4e/309a4ecd64fefa487ebb20c2eb23d922--fantasy-characters-female-characters.jpg",
+  "http://www.conceptart.org/forums/attachment.php?attachmentid=2239200&d=1490565589s"
+]
 
-  const rollStrength = document.querySelector('#rollStrength');
-  const rolledStrength = document.querySelector('#rolledStrength');
+// Utility functions
 
-  const rollDexerity = document.querySelector('#rollDexerity');
-  const rolledDexerity = document.querySelector('#rolledDexerity');
+const randomIntFromRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-  const rollConstitution = document.querySelector('#rollConstitution');
-  const rolledConstitition = document.querySelector('#rolledConstitition');
+const getCharacterImage = (genderedImages) => {
+  let randomIndex = randomIntFromRange(0, (genderedImages.length-1));
+  return genderedImages[randomIndex];
+}
 
-  const rollIntelligence = document.querySelector('#rollIntelligence');
-  const rolledIntelligence = document.querySelector('#rolledIntelligence');
+// create function to get ability score 3-18
 
-  const rollWisdom = document.querySelector('#rollWisdom');
-  const rolledWisdom = document.querySelector('#rolledWisdom');
+const abilityScore = () => Math.floor(Math.random() * ((18 - 3) + 1)) + 3;
 
-  const rollCharisma = document.querySelector('#rollCharisma');
-  const rolledCharisma = document.querySelector('#rolledCharisma');
+const setToMinMax = score => score > 18 ? score = 18 : score = 3;
 
-  const setScore = (scoreDisplay) => {
-    let score = abilityScore();
-    setToMinMax(score);
-    scoreDisplay.textContent = score.toString();
-  }
+// Declare Variables
 
-  rollStrength.addEventListener('click', () => {
-    setScore(rolledStrength);
-  });
+const rollStrength = document.querySelector('#rollStrength');
+const rolledStrength = document.querySelector('#rolledStrength');
 
-  rollDexerity.addEventListener('click', () => {
-    setScore(rolledDexerity);
-  });
+const rollDexerity = document.querySelector('#rollDexerity');
+const rolledDexerity = document.querySelector('#rolledDexerity');
 
-  rollConstitution.addEventListener('click', () => {
-    setScore(rolledConstitition);
-  });
+const rollConstitution = document.querySelector('#rollConstitution');
+const rolledConstitition = document.querySelector('#rolledConstitition');
 
-  rollWisdom.addEventListener('click', () => {
-    setScore(rolledWisdom);
-  });
+const rollIntelligence = document.querySelector('#rollIntelligence');
+const rolledIntelligence = document.querySelector('#rolledIntelligence');
 
-  rollIntelligence.addEventListener('click', () => {
-    setScore(rolledIntelligence);
-  });
+const rollWisdom = document.querySelector('#rollWisdom');
+const rolledWisdom = document.querySelector('#rolledWisdom');
 
-  rollCharisma.addEventListener('click', () => {
-    setScore(rolledCharisma);
-  });
+const rollCharisma = document.querySelector('#rollCharisma');
+const rolledCharisma = document.querySelector('#rolledCharisma');
 
-  const submitButton = document.querySelector('#submitButton');
+const setScore = (scoreDisplay) => {
+  let score = abilityScore();
+  setToMinMax(score);
+  scoreDisplay.textContent = score.toString();
+}
+
+rollStrength.addEventListener('click', () => {
+  setScore(rolledStrength);
+});
+
+rollDexerity.addEventListener('click', () => {
+  setScore(rolledDexerity);
+});
+
+rollConstitution.addEventListener('click', () => {
+  setScore(rolledConstitition);
+});
+
+rollWisdom.addEventListener('click', () => {
+  setScore(rolledWisdom);
+});
+
+rollIntelligence.addEventListener('click', () => {
+  setScore(rolledIntelligence);
+});
+
+rollCharisma.addEventListener('click', () => {
+  setScore(rolledCharisma);
+});
+
+const submitButton = document.querySelector('#submitButton');
 
 submitButton.addEventListener('click', () =>{
 
-    // Get info to create character
-    const $name = <HTMLInputElement>document.querySelector('#name');
+  // Get info to create character
 
-    const $race = <HTMLSelectElement>document.querySelector('#race');
-    const selectedRace = $race.options[$race.selectedIndex];
+  const $name = <HTMLInputElement>document.querySelector('#name');
 
-    const $strength = rolledStrength.textContent;
+  const $race = <HTMLSelectElement>document.querySelector('#race');
+  const selectedRace = $race.options[$race.selectedIndex];
 
-    const $dexerity = rolledDexerity.textContent;
+  const $strength = rolledStrength.textContent;
 
-    const $constitution = rolledConstitition.textContent;
+  const $dexerity = rolledDexerity.textContent;
 
-    const $intelligence = rolledIntelligence.textContent;
+  const $constitution = rolledConstitition.textContent;
 
-    const $wisdom = rolledWisdom.textContent;
+  const $intelligence = rolledIntelligence.textContent;
 
-    const $charisma = rolledCharisma.textContent;
+  const $wisdom = rolledWisdom.textContent;
 
-    const $alignment = <HTMLSelectElement>document.querySelector('#alignment');
-    const selectedAlignment = $alignment.options[$alignment.selectedIndex];
+  const $charisma = rolledCharisma.textContent;
 
-    const $cls = <HTMLSelectElement>document.querySelector('#cls');
-    const selectedCls = $cls.options[$cls.selectedIndex];
+  const $alignment = <HTMLSelectElement>document.querySelector('#alignment');
+  const selectedAlignment = $alignment.options[$alignment.selectedIndex];
 
-    const $gender = <HTMLInputElement>document.querySelector('#gender');
+  const $cls = <HTMLSelectElement>document.querySelector('#cls');
+  const selectedCls = $cls.options[$cls.selectedIndex];
 
-    const $age = <HTMLInputElement>document.querySelector('#age');
+  const $gender = <HTMLInputElement>document.querySelector('#gender');
 
-    // Post info from character creation to preview area
+  const $age = <HTMLInputElement>document.querySelector('#age');
 
-    const namePreview = document.querySelector('#namePreview');
-    namePreview.textContent = $name.value;
+  // Post info from character creation to preview area
 
-    const racePreview = <HTMLElement>document.querySelector('#racePreview');
-    racePreview.textContent = selectedRace.textContent;
+  const namePreview = document.querySelector('#namePreview');
+  namePreview.textContent = $name.value;
 
-    const genderPreview = <HTMLElement>document.querySelector('#genderPreview');
-    genderPreview.textContent = $gender.value;
+  const racePreview = <HTMLElement>document.querySelector('#racePreview');
+  racePreview.textContent = selectedRace.textContent;
 
-<<<<<<< HEAD
-    const agePreview = <HTMLInputElement>document.querySelector('#agePreview');
-    agePreview.textContent = $age.value;
+  const genderPreview = <HTMLElement>document.querySelector('#genderPreview');
+  genderPreview.textContent = $gender.value;
 
-    // stats, align, class
-=======
-    const agePreview = <HTMLElement>document.querySelector('#agePreview');
-    agePreview.textContent = $age.value;
->>>>>>> 6b37e056d86acb43531fa1e58f0b2d5e3fa1be3e
+  const agePreview = <HTMLElement>document.querySelector('#agePreview');
+  agePreview.textContent = $age.value;
 
-    const strengthPreview = <HTMLElement>document.querySelector('#strengthPreview');
-    strengthPreview.textContent = $strength;
+  const strengthPreview = <HTMLElement>document.querySelector('#strengthPreview');
+  strengthPreview.textContent = $strength;
 
-    const dexerityPreview = <HTMLElement>document.querySelector('#dexerityPreview');
-    dexerityPreview.textContent = $dexerity;
+  const dexerityPreview = <HTMLElement>document.querySelector('#dexerityPreview');
+  dexerityPreview.textContent = $dexerity;
 
-    const constitutionPreview = <HTMLElement>document.querySelector('#constitutionPreview');
-    constitutionPreview.textContent = $constitution;
+  const constitutionPreview = <HTMLElement>document.querySelector('#constitutionPreview');
+  constitutionPreview.textContent = $constitution;
 
-    const wisdomPreview = <HTMLElement>document.querySelector('#wisdomPreview');
-    wisdomPreview.textContent = $wisdom;
+  const wisdomPreview = <HTMLElement>document.querySelector('#wisdomPreview');
+  wisdomPreview.textContent = $wisdom;
 
-    const intelligencePreview = <HTMLElement>document.querySelector('#intelligencePreview');
-    intelligencePreview.textContent = $intelligence;
+  const intelligencePreview = <HTMLElement>document.querySelector('#intelligencePreview');
+  intelligencePreview.textContent = $intelligence;
 
-    const charismaPreview = <HTMLElement>document.querySelector('#charismaPreview');
-    charismaPreview.textContent = $charisma;
+  const charismaPreview = <HTMLElement>document.querySelector('#charismaPreview');
+  charismaPreview.textContent = $charisma;
 
-    const clsPreview = <HTMLElement>document.querySelector('#clsPreview');
-    clsPreview.textContent = selectedCls.textContent;
+  const clsPreview = <HTMLElement>document.querySelector('#clsPreview');
+  clsPreview.textContent = selectedCls.textContent;
 
-    const alignmentPreview = <HTMLElement>document.querySelector('#alignmentPreview');
-    alignmentPreview.textContent = selectedAlignment.textContent;
+  const alignmentPreview = <HTMLElement>document.querySelector('#alignmentPreview');
+  alignmentPreview.textContent = selectedAlignment.textContent;
 
-  });
+  // Get character preview image based on class and gender
+
+  if($gender.value.toLowerCase() === 'male') {
+
+    // class if statement for male genders
+
+    const characterImg = <HTMLImageElement>document.querySelector('#characterImg');
+    characterImg.src = getCharacterImage(maleCharacterImages);
+    console.log(characterImg.src)
+
+  } else if($gender.value.toLowerCase() === 'female') {
+
+    const characterImg = <HTMLImageElement>document.querySelector('#characterImg');
+    characterImg.src = getCharacterImage(femaleCharacterImages);
+    console.log(characterImg.src)
+
+  } else {
+
+    const characterImg = <HTMLImageElement>document.querySelector('#characterImg');
+    let randomBoolean = Math.random() >= 0.5;
+    if(randomBoolean) {
+      characterImg.src = getCharacterImage(maleCharacterImages);
+    } else if (!randomBoolean) {
+      characterImg.src = getCharacterImage(femaleCharacterImages);
+    }
+    console.log(randomBoolean)
+
+  }
+
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,26 @@
-// create function to get ability score 3-18
-var abilityScore = function () {
-    return Math.floor(Math.random() * ((18 - 3) + 1)) + 3;
+// Character image array
+var maleCharacterImages = [
+    "https://i.pinimg.com/736x/86/84/32/868432784e4fd37beb893ecdb0830a74--old-friends-barbarian-fantasy.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWmOrOMDeUgAgx9IWjCZD9g5oBec8-fk8xZH9UuofS_VAOt3zb",
+    "https://i.pinimg.com/736x/08/69/71/086971ec519d01df20ae9cb4bf5e411d--character-outfits-character-portraits.jpg",
+    "https://www.videogamesartwork.com/sites/default/files/images/image/1377178542/diablo3-class-barbarian-male-06.jpg",
+    "https://i.pinimg.com/736x/bd/4f/b9/bd4fb957e6046747533e6d62af90f6fe--fantasy-warrior-fantasy-art.jpg"
+];
+var femaleCharacterImages = [
+    "https://vignette.wikia.nocookie.net/bgtscc/images/9/99/Barbarian.jpg/revision/latest?cb=20161105214456",
+    "https://i.kinja-img.com/gawker-media/image/upload/s--NdbChKcZ--/c_scale,f_auto,fl_progressive,q_80,w_800/18j3zwhxy85iyjpg.jpg",
+    "https://cdna.artstation.com/p/assets/covers/images/001/568/198/large/denny-ibnu-barbarian-final-lr.jpg?1448720934",
+    "https://i.pinimg.com/236x/30/9a/4e/309a4ecd64fefa487ebb20c2eb23d922--fantasy-characters-female-characters.jpg",
+    "http://www.conceptart.org/forums/attachment.php?attachmentid=2239200&d=1490565589s"
+];
+// Utility functions
+var randomIntFromRange = function (min, max) { return Math.floor(Math.random() * (max - min + 1) + min); };
+var getCharacterImage = function (genderedImages) {
+    var randomIndex = randomIntFromRange(0, (genderedImages.length - 1));
+    return genderedImages[randomIndex];
 };
+// create function to get ability score 3-18
+var abilityScore = function () { return Math.floor(Math.random() * ((18 - 3) + 1)) + 3; };
 var setToMinMax = function (score) { return score > 18 ? score = 18 : score = 3; };
 // Declare Variables
 var rollStrength = document.querySelector('#rollStrength');
@@ -55,11 +74,7 @@ submitButton.addEventListener('click', function () {
     var selectedAlignment = $alignment.options[$alignment.selectedIndex];
     var $cls = document.querySelector('#cls');
     var selectedCls = $cls.options[$cls.selectedIndex];
-<<<<<<< HEAD
-    var $gender = document.querySelector('#gender').textContent;
-=======
     var $gender = document.querySelector('#gender');
->>>>>>> 6b37e056d86acb43531fa1e58f0b2d5e3fa1be3e
     var $age = document.querySelector('#age');
     // Post info from character creation to preview area
     var namePreview = document.querySelector('#namePreview');
@@ -70,10 +85,6 @@ submitButton.addEventListener('click', function () {
     genderPreview.textContent = $gender.value;
     var agePreview = document.querySelector('#agePreview');
     agePreview.textContent = $age.value;
-<<<<<<< HEAD
-    // stats, align, class
-=======
->>>>>>> 6b37e056d86acb43531fa1e58f0b2d5e3fa1be3e
     var strengthPreview = document.querySelector('#strengthPreview');
     strengthPreview.textContent = $strength;
     var dexerityPreview = document.querySelector('#dexerityPreview');
@@ -90,4 +101,27 @@ submitButton.addEventListener('click', function () {
     clsPreview.textContent = selectedCls.textContent;
     var alignmentPreview = document.querySelector('#alignmentPreview');
     alignmentPreview.textContent = selectedAlignment.textContent;
+    // Get character preview image based on class and gender
+    if ($gender.value.toLowerCase() === 'male') {
+        // class if statement for male genders
+        var characterImg = document.querySelector('#characterImg');
+        characterImg.src = getCharacterImage(maleCharacterImages);
+        console.log(characterImg.src);
+    }
+    else if ($gender.value.toLowerCase() === 'female') {
+        var characterImg = document.querySelector('#characterImg');
+        characterImg.src = getCharacterImage(femaleCharacterImages);
+        console.log(characterImg.src);
+    }
+    else {
+        var characterImg = document.querySelector('#characterImg');
+        var randomBoolean = Math.random() >= 0.5;
+        if (randomBoolean) {
+            characterImg.src = getCharacterImage(maleCharacterImages);
+        }
+        else if (!randomBoolean) {
+            characterImg.src = getCharacterImage(femaleCharacterImages);
+        }
+        console.log(randomBoolean);
+    }
 });
