66e5ba246f2f0276f62d54e74adb5fd97923511e
Jeremy Bratcher <jeremybratcher@gmail.com>
2018-08-08 11:42:23 +0000

    added app.ts to handle active class changing on navs, put creation area into a bootstrap card, improved spacing and padding

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Help bootstrap change active class of menu item
+$('#charTabs a').on('click', function () {
+    $('#charTabs').find('li.active').removeClass('active');
+    $(this).parent('li').addClass('active');
+});
---
+++ src/ts/app.ts
@@ -0,0 +1,6 @@
+// Help bootstrap change active class of menu item
+
+$( '#charTabs a' ).on( 'click', function () {
+	$( '#charTabs' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});
